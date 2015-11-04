0a97ef11a2903124af91a91a5016100821eafea7
Yev Dyko <evgeny.dyko@gmail.com>
2015-11-04 15:09:21 +0300

    Fixed DataMapper db configuration

--- app/models/link.rb
+++ app/models/link.rb
@@ -9,6 +9,7 @@ class Link
   property :url, String
 end
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] ||"postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
