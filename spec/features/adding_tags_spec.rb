af6bacf0a3315f48d6aebb03595102c074076e9d
Yev Dyko <evgeny.dyko@gmail.com>
2015-11-04 21:06:02 +0300

    Added multiply tags

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -8,4 +8,14 @@ feature 'Adding tags' do
     link = Link.first
     expect(link.tags.map(&:name)).to include('bootcamp')
   end
+
+  scenario 'User can add multiply tags to the links' do
+    visit 'links/new'
+    fill_in :url, with: 'http://www.makersacademy.com'
+    fill_in :title, with: 'Makers Academy'
+    fill_in :tags, with: 'bootcamp education'
+    click_button 'Add bookmark'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('bootcamp', 'education')
+  end
 end
